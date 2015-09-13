ae0b82e3386414e66ac490628b27ec9fe1881196
Kent <kent@studiofreya.com>
2015-09-14 00:49:33 +0200

    Updated links

---
+++ samples/boost-format-str/README.md
@@ -0,0 +1,4 @@
+= Boost format string examples =
+
+Code samples used in http://studiofreya.com/cpp/boost/boost-format-string-examples/
+
--- samples/boost-format-str/boost-format-str.cpp
+++ samples/boost-format-str/boost-format-str.cpp
@@ -1,20 +1,32 @@
+// Code on Github:
+// https://github.com/Studiofreya/code-samples/blob/master/samples/boost-format-str/boost-format-str.cpp
+//
+// Post with code:
+// http://studiofreya.com/cpp/boost/boost-format-string-examples/
+
 #include <boost/format.hpp>
 
 int main()
 {
-	boost::format fmt("Pi is %4.2f");
-	fmt % 3.14;
+	// Create a boost::format object, 
+	boost::format formatobject("Pi is %4.2f");
 
-	// Copy of string
-	std::string s1 = boost::str(fmt);
-	std::string s2 = fmt.str();
+	// Feed it with Pi
+	formatobject % 3.14;
+					 
+	// Get strings
+	std::string str1 = boost::str(formatobject);
+	std::string str2 = formatobject.str();
 
 	// If a constant value can be used,
 	// use a const ref. This is legal C++ code.
 	// See "The most important const":
 	// http://herbsutter.com/2008/01/01/gotw-88-a-candidate-for-the-most-important-const/
-	const std::string & s3 = boost::str(fmt);
-	const std::string & s4 = fmt.str();
+	const std::string & str3 = boost::str(formatobject);
+	const std::string & str4 = formatobject.str();
+
+	// Character array from the first string
+	const char * char1 = str1.c_str();
 
 	return 0;
 }
